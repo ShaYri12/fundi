5c638a21c9d43aa68264055b03eec9b5a608ed67
ShaYri12 <sharjeelb60@gmail.com>
2024-11-06 02:05:06 +0500

    some styling fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-night-black mt-[80px]">
+    <footer className="bg-night-black">
       <div className="py-10 border-b border-white border-opacity-50 px-4">
         <div className="max-w-[1440px] w-full mx-auto">
           <div>
@@ -32,25 +32,37 @@ const Footer = () => {
           <div className="mt-7 flex justify-center">
             <ul className="flex flex-wrap gap-4">
               <li>
-                <Link to="#" className="flex items-center justify-center h-10 sm:h-11 w-10 sm:w-11 rounded-full border border-white/80 transition-[background-color] duration-200 hover:bg-white/80">
+                <Link
+                  to="#"
+                  className="flex items-center justify-center h-10 sm:h-11 w-10 sm:w-11 rounded-full border border-white/80 transition-[background-color] duration-200 hover:bg-white/80"
+                >
                   <img src={Facebook} alt="Facebook" className="h-[22px]" />
                 </Link>
               </li>
 
               <li>
-                <Link to="#" className="flex items-center justify-center h-10 sm:h-11 w-10 sm:w-11 rounded-full border border-white/80 transition-[background-color] duration-200 hover:bg-white/80">
+                <Link
+                  to="#"
+                  className="flex items-center justify-center h-10 sm:h-11 w-10 sm:w-11 rounded-full border border-white/80 transition-[background-color] duration-200 hover:bg-white/80"
+                >
                   <img src={Linkden} alt="Linkden" className="h-5" />
                 </Link>
               </li>
 
               <li>
-                <Link to="#" className="flex items-center justify-center h-10 sm:h-11 w-10 sm:w-11 rounded-full border border-white/80 transition-[background-color] duration-200 hover:bg-white/80">
+                <Link
+                  to="#"
+                  className="flex items-center justify-center h-10 sm:h-11 w-10 sm:w-11 rounded-full border border-white/80 transition-[background-color] duration-200 hover:bg-white/80"
+                >
                   <img src={Instagram} alt="Instagram" className="h-5" />
                 </Link>
               </li>
 
               <li>
-                <Link to="#" className="flex items-center justify-center h-10 sm:h-11 w-10 sm:w-11 rounded-full border border-white/80 transition-[background-color] duration-200 hover:bg-white/80">
+                <Link
+                  to="#"
+                  className="flex items-center justify-center h-10 sm:h-11 w-10 sm:w-11 rounded-full border border-white/80 transition-[background-color] duration-200 hover:bg-white/80"
+                >
                   <img src={Youtube} alt="Youtube" className="h-[17px]" />
                 </Link>
               </li>
@@ -59,20 +71,34 @@ const Footer = () => {
         </div>
       </div>
 
-
       <div className="py-10 px-4">
         <div className="max-w-[1440px] w-full mx-auto">
           <ul className="flex flex-col gap-y-3">
             <li className="">
-              <Link href="#" className="inline-block font-axiforma text-normal sm:text-[22px] text-white relative transition-[padding] duration-300 hover:pl-12 before:content-[url(https://5255565.fs1.hubspotusercontent-na1.net/hubfs/5255565/mews01/Global/Icons/white-footer-link-icon.svg)] before:absolute before:top-1/2 before:-translate-y-1/2 before:left-0 before:leading-[1px] before:opacity-0 before:transition-[opacity] before:duration-300 hover:before:opacity-100">Features</Link>
+              <Link
+                href="#"
+                className="inline-block font-axiforma text-normal sm:text-[22px] text-white relative transition-[padding] duration-300 hover:pl-12 before:content-[url(https://5255565.fs1.hubspotusercontent-na1.net/hubfs/5255565/mews01/Global/Icons/white-footer-link-icon.svg)] before:absolute before:top-1/2 before:-translate-y-1/2 before:left-0 before:leading-[1px] before:opacity-0 before:transition-[opacity] before:duration-300 hover:before:opacity-100"
+              >
+                Features
+              </Link>
             </li>
 
             <li>
-              <Link href="#" className="inline-block font-axiforma text-normal sm:text-[22px] text-white relative transition-[padding] duration-300 hover:pl-12 before:content-[url(https://5255565.fs1.hubspotusercontent-na1.net/hubfs/5255565/mews01/Global/Icons/white-footer-link-icon.svg)] before:absolute before:top-1/2 before:-translate-y-1/2 before:left-0 before:leading-[1px] before:opacity-0 before:transition-[opacity] before:duration-300 hover:before:opacity-100">Pricing </Link>
+              <Link
+                href="#"
+                className="inline-block font-axiforma text-normal sm:text-[22px] text-white relative transition-[padding] duration-300 hover:pl-12 before:content-[url(https://5255565.fs1.hubspotusercontent-na1.net/hubfs/5255565/mews01/Global/Icons/white-footer-link-icon.svg)] before:absolute before:top-1/2 before:-translate-y-1/2 before:left-0 before:leading-[1px] before:opacity-0 before:transition-[opacity] before:duration-300 hover:before:opacity-100"
+              >
+                Pricing{" "}
+              </Link>
             </li>
 
             <li>
-              <Link href="#"  className="inline-block font-axiforma text-normal sm:text-[22px] text-white relative transition-[padding] duration-300 hover:pl-12 before:content-[url(https://5255565.fs1.hubspotusercontent-na1.net/hubfs/5255565/mews01/Global/Icons/white-footer-link-icon.svg)] before:absolute before:top-1/2 before:-translate-y-1/2 before:left-0 before:leading-[1px] before:opacity-0 before:transition-[opacity] before:duration-300 hover:before:opacity-100">FAQ</Link>
+              <Link
+                href="#"
+                className="inline-block font-axiforma text-normal sm:text-[22px] text-white relative transition-[padding] duration-300 hover:pl-12 before:content-[url(https://5255565.fs1.hubspotusercontent-na1.net/hubfs/5255565/mews01/Global/Icons/white-footer-link-icon.svg)] before:absolute before:top-1/2 before:-translate-y-1/2 before:left-0 before:leading-[1px] before:opacity-0 before:transition-[opacity] before:duration-300 hover:before:opacity-100"
+              >
+                FAQ
+              </Link>
             </li>
           </ul>
         </div>
--- src/pages/home/MakeItRemarkable.jsx
+++ src/pages/home/MakeItRemarkable.jsx
@@ -4,31 +4,33 @@ import makeItRemarkable from "../../assets/make-it-remarkable.webp";
 const MakeItRemarkable = () => {
   return (
     <div className="bg-[#F8F5F2]">
-      <div className="bg-[#E6F5F0] flex items-center md:rounded-t-[40px] lg:rounded-t-[80px] py-[4rem]">
-        <div className="container mx-auto px-4 lg:px-8 flex flex-col md:flex-row items-center md:gap-[4rem] lg:gap-[8rem]">
-          <div className="w-full md:w-[50%] lg:w-1/2 sm:flex hidden">
-            <div className="relative w-full overflow-hidden">
-              <img
-                src={makeItRemarkable}
-                alt="Vintage hotel interior with chairs and tables"
-                className="image rounded-3xl lg:rounded-[40px] aspect-[464/416] object-cover lg:w-full h-full hidden md:flex lg:flex"
-              />
+      <div className="bg-[#E6F5F0] flex items-center md:rounded-t-[40px] lg:rounded-t-[80px] py-[6rem]">
+        <div className="container mx-auto px-4 lg:px-8">
+          <div className="max-w-[1062px] mx-auto flex flex-col md:flex-row items-center md:gap-[4rem] lg:gap-[7rem]">
+            <div className="w-full md:w-[50%] lg:w-1/2 sm:flex hidden">
+              <div className="relative w-full overflow-hidden">
+                <img
+                  src={makeItRemarkable}
+                  alt="Vintage hotel interior with chairs and tables"
+                  className="image rounded-3xl lg:rounded-[40px] aspect-[464/416] object-cover lg:w-full h-full hidden md:flex lg:flex"
+                />
+              </div>
             </div>
-          </div>
 
-          <div className="w-full md:w-[50%] lg:w-5/12 flex flex-col gap-[1.5rem]">
-            <h1 className="text-[32px] sm:text-[48px] font-[400] font-axiformaa tracking-tight text-[#1c1d24] leading-[125%]">
-              Make it <br className="md:flex hidden" /> remarkable.
-            </h1>
+            <div className="w-full md:w-[50%] lg:w-5/12 flex flex-col gap-[1.5rem]">
+              <h1 className="text-[32px] sm:text-[48px] font-[400] font-axiformaa tracking-tight text-[#1c1d24] leading-[125%]">
+                Make it <br className="md:flex hidden" /> remarkable.
+              </h1>
 
-            <p className="text-[#494951] max-w-lg">
-              Ready to take the first step towards a more efficient, flexible,
-              guest-centric approach to hospitality?
-            </p>
+              <p className="text-[#494951] max-w-lg">
+                Ready to take the first step towards a more efficient, flexible,
+                guest-centric approach to hospitality?
+              </p>
 
-            <button className="bg-[#ff5a65] hover:bg-[#ff3d5a] text-[15px] text-white font-medium py-[0.625rem] px-[1.5rem] rounded-full transition-colors flex items-center gap-2 w-fit">
-              Book a demo
-            </button>
+              <button className="bg-[#ff5a65] hover:bg-[#ff3d5a] text-[15px] text-white font-medium py-[0.625rem] px-[1.5rem] rounded-full transition-colors flex items-center gap-2 w-fit">
+                Book a demo
+              </button>
+            </div>
           </div>
         </div>
       </div>
